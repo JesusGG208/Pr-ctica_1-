944cbc0d6850b3769c6fc94eba6a7a93b0e8b089
EsDsVandal <jgomgar208@g.educaand.es>
2024-12-11 00:09:09 +0100

    ejercicio 3 no comentado todavía

--- ejercicio_3/ejercicio_3.js
+++ ejercicio_3/ejercicio_3.js
@@ -1,18 +1,43 @@
+document.getElementById("descifrar").addEventListener("click", () => { cifradoCesar(); });
+
 function cifradoCesar() {
     let idiomaAlfabeto = document.getElementById("alfabeto");
-    let textoCifrado = document.getElementById("texto");
-    let numeroRotacion = document.getElementById("rotacion");
+    let textoCifrado = document.getElementById("texto").value;
+    let numeroRotacion = parseInt(document.getElementById("rotacion").value);
 
     let alfabetoIngles = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let alfabetoEspanol = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
 
-    if (idiomaAlfabeto.value === castellano) {
-        alert(descifrar(idiomaAlfabeto, textoCifrado, numeroRotacion));
-    } else if (idiomaAlfabeto.value === ingles) {
-        alert(descifrar(idiomaAlfabeto, textoCifrado, numeroRotacion));
+    if (idiomaAlfabeto.value === "castellano") {
+        let mensaje = descifrar(alfabetoEspanol, textoCifrado, numeroRotacion);
+        document.getElementById("resultado").innerText = mensaje;
+    } else if (idiomaAlfabeto.value === "ingles") {
+        let mensaje = descifrar(alfabetoIngles, textoCifrado, numeroRotacion);
+        document.getElementById("resultado").innerText = mensaje;
     }
 }
 
-function descifrar(alfabeto, texto, rota) {
+function descifrar(alfabeto, texto, rotacion) {
     let mensajeDescifrado = "";
+
+    for (let i = 0; i < texto.length; i++) {
+        let caracter = texto.charAt(i);
+
+        if (alfabeto.includes(caracter)) {
+            for (let j = 0; j < alfabeto.length; j++) {
+                if (caracter === alfabeto.charAt(j)) {
+                    let nuevoCaracter = j - rotacion;
+                    if (nuevoCaracter < 0) {
+                        nuevoCaracter += alfabeto.length;
+                    }
+                    mensajeDescifrado += alfabeto.charAt(nuevoCaracter);
+                    break;
+                }
+            }
+        } else {
+            mensajeDescifrado += caracter;
+        }
+    }
+
+    return mensajeDescifrado;
 }
